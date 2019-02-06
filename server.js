974f3213f19f6f8649d0432f4e81e0ae259f2aa2
ThomasEmilsson <gusemith@student.gu.se>
2019-02-06 02:17:31 +0100

    working version

--- server.js
+++ server.js
@@ -2,20 +2,26 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var bodyParser = require('body-parser')
+var multer = require('multer')
 
 var app = express();
+var upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+  next();
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
